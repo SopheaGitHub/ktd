ba0aa5a0c987394b472873e66ed9c3ce649ec114
@1Sophea2Password <chansophea9@gmail.com>
2018-02-13 22:17:48 +0700

    change login

--- login.php
+++ login.php
@@ -15,24 +15,24 @@
 	    	<div class="col-md-4">
 	    		<form class="form-horizontal">
 				  	<div class="form-group">
-				    <label for="" class="col-sm-3 control-label">Email</label>
-				    <div class="col-sm-9">
-				      <input type="email" class="form-control" placeholder="Email" autofocus />
+				    <label for="" class="col-sm-5 control-label">Phone Number Or Email</label>
+				    <div class="col-sm-7">
+				      <input type="email" class="form-control"  placeholder="Phone Number Or Email" />
 				    </div>
 				  	</div>
 				  	<div class="form-group">
-				    <label for="" class="col-sm-3 control-label">Password</label>
-				    <div class="col-sm-9">
+				    <label for="" class="col-sm-5 control-label">Password</label>
+				    <div class="col-sm-7">
 				      <input type="password" class="form-control"  placeholder="Password" />
 				    </div>
 				  	</div>
 				  	<div class="form-group">
-				    <div class="col-sm-offset-3 col-sm-9">
+				    <div class="col-sm-offset-5 col-sm-7">
 				    	<a href="">Forgot your password?</a>
 				    </div>
 				  	</div>
 				  	<div class="form-group">
-				    <div class="col-sm-offset-3 col-sm-9">
+				    <div class="col-sm-offset-5 col-sm-7">
 				      <button type="submit" class="btn btn-success">Log in</button>
 				    </div>
 				  	</div>
